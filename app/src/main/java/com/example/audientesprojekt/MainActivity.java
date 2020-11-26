5c144bdf10bebebad260ec24fdeb73abe0b43abc
Sid-Ali-Mounib <sidali-m@hotmail.com>
2020-11-26 11:24:32 +0100

    login button

--- app/src/main/java/com/example/audientesprojekt/MainActivity.java
+++ app/src/main/java/com/example/audientesprojekt/MainActivity.java
@@ -7,22 +7,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button secondActivityBtn ;
+    Button loginButton ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button secondActivityBtn = (Button) findViewById(R.id.second_activity_btn);
-        secondActivityBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent startIntent = new Intent(getApplicationContext(), Menu_Activity.class);
-                startActivity(startIntent);
-            }
+        secondActivityBtn = findViewById(R.id.second_activity_btn);
+        loginButton = findViewById(R.id.loginButton);
+        secondActivityBtn.setOnClickListener(this);
+        loginButton.setOnClickListener(this);
 
-        });
 
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == secondActivityBtn){
+            Intent a = new Intent(this, Menu_Activity.class);
+            startActivity(a);
+        }else {
+            Intent a = new Intent(this, Menu_Activity.class);
+            startActivity(a);
+        }
     }
 }

a30c078ccf3fde4022b6148529f2cf753e5c784a
Thorsager1 <s195470@student.dtu.dk>
2021-01-05 16:41:51 +0100

    det lykkedes at slette og stadig få det til at virke. prøver at få hentet filer ind i mit recyclerview i library nu.

--- app/src/main/java/com/example/audientesprojekt/libraryFragment.java
+++ app/src/main/java/com/example/audientesprojekt/libraryFragment.java
@@ -3,11 +3,15 @@ package com.example.audientesprojekt;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.io.File;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,12 +20,27 @@ import android.widget.TextView;
  */
 public class libraryFragment extends Fragment {
 
+    String path = "C:/Users/mikke/AndroidStudioProjects/AudientesProjekt/app/src/main/res/raw/Animals-flac";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        GetFiles();
+
+
         return inflater.inflate(R.layout.fragment_library, container, false);
+    }
 
+    private ArrayList<String> GetFiles() {
+        ArrayList<String> filenames = new ArrayList<String>();
 
+        File directory = new File(path);
+        File[] files = directory.listFiles();
 
+        for(int i = 0; i < files.length; i++){
+            String file_name = files[i].getName();
+            filenames.add(file_name);
+        }
+        return filenames;
     }
 }

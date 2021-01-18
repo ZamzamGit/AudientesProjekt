adc0f56fbff279e0299afa522345e22ab607ebba
Sid-Ali-Mounib <sidali-m@hotmail.com>
2021-01-18 15:43:21 +0100

    Button Link

--- app/src/main/java/com/example/audientesprojekt/HearingTestFragment.java
+++ app/src/main/java/com/example/audientesprojekt/HearingTestFragment.java
@@ -1,5 +1,8 @@
 package com.example.audientesprojekt;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,16 +12,33 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the  factory method to
  * create an instance of this fragment.
  */
-public class HearingTestFragment extends Fragment {
+
+public class HearingTestFragment extends Fragment implements View.OnClickListener {
+
+    Button hearingTest;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_hearing_test, container,false);
+        View v = inflater.inflate(R.layout.fragment_hearing_test, container,false);
+
+        hearingTest = v.findViewById(R.id.button);
+        hearingTest.setOnClickListener(this);
+        return v;
+    }
+
+    public void onClick(View v) {
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=dk.alen.audientes")));
+        } catch (ActivityNotFoundException e){
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=dk.alen.audientes&hl=en_IN%22")));
+        }
     }
 }

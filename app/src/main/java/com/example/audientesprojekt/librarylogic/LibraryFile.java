182d7bbc033e6e9c00996b5ba213143f77d60428
khaledzamzam <khaled-yahia-zamzam@hotmail.com>
2021-01-20 11:26:06 +0100

    library finish

--- app/src/main/java/com/example/audientesprojekt/librarylogic/LibraryFile.java
+++ app/src/main/java/com/example/audientesprojekt/librarylogic/LibraryFile.java
@@ -9,7 +9,6 @@ public class LibraryFile {
     public LibraryFile(String fileName, Uri uri) {
         this.fileName = fileName;
         this.uri = uri;
-
     }
 
     public String getFileName() {

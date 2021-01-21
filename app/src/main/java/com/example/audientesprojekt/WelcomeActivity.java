6cb7dd19041ad9e5ebe28b8bea50f2481feffce9
khaledzamzam <khaled-yahia-zamzam@hotmail.com>
2021-01-21 03:43:40 +0100

    done

---
+++ app/src/main/java/com/example/audientesprojekt/WelcomeActivity.java
@@ -0,0 +1,27 @@
+package com.example.audientesprojekt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Button button = findViewById(R.id.welcomeBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
